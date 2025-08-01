0a7a1539d038574da42ee13a6513e50e6c1d070c
Ryan Mar <Phyre205@gmail.com>
2025-07-31 22:34:58 -0700

    a

---
+++ glory-web/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbpDrq1MylISg1DqQep6mlONFsdYPWG-k",
+  authDomain: "glory-crafting.firebaseapp.com",
+  projectId: "glory-crafting",
+  storageBucket: "glory-crafting.firebasestorage.app",
+  messagingSenderId: "223383939037",
+  appId: "1:223383939037:web:ee53bc3d35d7c36ec37834",
+  measurementId: "G-R8C2XVJTFR"
+};
+
+const app = initializeApp(firebaseConfig);
